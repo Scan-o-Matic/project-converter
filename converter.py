ac3add4870d8a8d9bac475c9cca8b2eea9438b74
Martin Zackrisson <martin.zackrisson@molflow.com>
2022-01-11 14:56:05 +0100

    Fix reviewer comments

--- converter.py
+++ converter.py
@@ -20,31 +20,35 @@ def find_files(directory, pattern):
                 yield filename
 
 
-l = Logger('Converter')
+logger = Logger('Converter')
 BASE_DIR = '/projects'
-p = Paths()
+paths = Paths()
 
 for file_type, pattern, loader, processor in (
     (
         'project compilations',
-        p.project_compilation_pattern.format('*'),
+        paths.project_compilation_pattern.format('*'),
         CompileImageAnalysisFactory.serializer.load,
         lambda data: sorted(data, key=lambda e: e.image.index),
     ),
     (
         'project compilation instructions',
-        p.project_compilation_instructions_pattern.format('*'),
+        paths.project_compilation_instructions_pattern.format('*'),
         CompileProjectFactory.serializer.load_first,
         None,
     ),
 ):
-    l.info('Converting all {} in {}'.format(file_type, BASE_DIR))
+    logger.info('Converting all {} in {}'.format(file_type, BASE_DIR))
     n = 0
     for fname in find_files(BASE_DIR, pattern):
         try:
             with open(fname) as fh:
                 if json.load(fh):
-                    l.info("Skipping {} because seems already converted".format(fname))
+                    logger.info(
+                        'Skipping {} because seems already converted'.format(
+                            fname,
+                        ),
+                    )
                     continue
         except ValueError:
             pass
@@ -52,7 +56,10 @@ for file_type, pattern, loader, processor in (
         data = loader(fname)
         if processor is not None:
             data = processor(data)
-        l.info('Converting {} ({} entries)'.format(fname, len(data) if isinstance(data, list) else 1))
+        logger.info('Converting {} ({} entries)'.format(
+            fname,
+            len(data) if isinstance(data, list) else 1,
+        ))
         jsonizer.dump(data, fname)
         n += 1
-    l.info('Converted {} {} files'.format(n, file_type))
+    logger.info('Converted {} {} files'.format(n, file_type))
