29f9af571252b69eab6e3758c4e96a50a769d429
Andreas Skyman <andreas.skyman@molflow.com>
2022-01-24 16:22:29 +0100

    log when nothing to do

--- converter.py
+++ converter.py
@@ -105,11 +105,13 @@ for file_type, pattern, loader, processor, new_ext in (
             fname,
             len(data) if isinstance(data, list) else int(data is not None),
         ))
+        if data is None:
+            logger.error('Found nothing to convert in {}'.format(fname))
+            continue
 
         dump_name = get_dump_name(fname, new_ext)
-        if data is not None:
-            jsonizer.dump(data, get_dump_name(fname, new_ext))
-            if dump_name != fname:
-                os.remove(fname)
-            n += 1
+        jsonizer.dump(data, get_dump_name(fname, new_ext))
+        if dump_name != fname:
+            os.remove(fname)
+        n += 1
     logger.info('Converted {} {} files'.format(n, file_type))
